020d992f08ecd97999173ad625fe7a2074cc3dd8
justinsthomas <justin.stephen.thomas@gmail.com>
2013-12-04 13:23:20 -0500

    added greatGrandparent

--- js/jquery.familytree.js
+++ js/jquery.familytree.js
@@ -2,3 +2,8 @@ $.fn.grandparent = function(){
     var el = $( this ).parent().parent();
     return el;
 }
+
+$.fn.greatGrandparent = function(){
+    var el = $( this ).parent().parent().parent();
+    return el;
+}
